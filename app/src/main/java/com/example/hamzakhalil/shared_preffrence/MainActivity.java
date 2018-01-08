e8cd455777e7136fb0835c773ace69515effb1ee
hamza khalil <usmankhan4858180@gmail.com>
2018-01-08 16:07:02 +0500

    Initial commit

---
+++ app/src/main/java/com/example/hamzakhalil/shared_preffrence/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hamzakhalil.shared_preffrence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
